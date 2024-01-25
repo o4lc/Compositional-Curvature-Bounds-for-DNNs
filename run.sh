0c12ba801a51b115f4bdb69822c2dda9274bf3f2
sinnna <sharifi.sina1377@gmail.com>
2024-01-25 13:02:37 -0500

    plotting improved

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+
 gpu=0
 
+dataset=mnist
+model_name=xsmall
+dir="$dataset/$model_name"
+activation=tanh
 
-# CUDA_VISIBLE_DEVICES=$gpu torchrun --standalone --nnodes=1 --nproc_per_node=gpu main.py --dataset cifar10\
-#             --model-name xsmall --train_dir c10/xsmall --epochs 500 --batch_size 256 --activation tanh\
-#             --penalizeCurvature True
+CUDA_VISIBLE_DEVICES=$gpu torchrun --standalone --nnodes=1 --nproc_per_node=gpu main.py --dataset $dataset \
+             --model-name $model_name --train_dir $dir --epochs 250 --batch_size 256 --activation $activation \
+             --penalizeCurvature --hessianRegularizerPrimalDualEpsilon 0.75
 
 
 
-CUDA_VISIBLE_DEVICES=$gpu python3 main.py --mode certified --dataset cifar10 --model-name xsmall --train_dir c10/xsmall --activation tanh
+CUDA_VISIBLE_DEVICES=$gpu python3 main.py --mode certified --dataset $dataset --model-name $model_name\
+             --train_dir $dir --activation $activation
